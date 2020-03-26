015e1eca7bf0bd7db4f4c48a307253624ec98072
Alex St.Clair <tug89508@temple.edu>
2020-03-26 13:18:39 -0400

    cleaned some code and added the interface

--- app/src/main/java/edu/temple/bookshelf/ListFragment.java
+++ app/src/main/java/edu/temple/bookshelf/ListFragment.java
@@ -15,7 +15,7 @@ import java.util.HashMap;
 
 
 /**
- * A simple {@link Fragment} subclass.
+ * A simple {@link Fragment} subclass that shows a list of items
  * Use the {@link ListFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
@@ -24,7 +24,6 @@ public class ListFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "list";
 
-    // TODO: Rename and change types of parameters
     private ArrayList<HashMap<String, String>> list;
 
     // Required empty public constructor
@@ -41,6 +40,8 @@ public class ListFragment extends Fragment {
     public static ListFragment newInstance(ArrayList<HashMap<String, String>> param1) {
         ListFragment fragment = new ListFragment();
         Bundle args = new Bundle();
+
+        //put the list into the bundle for this fragment
         args.putSerializable(ARG_PARAM1, param1);
         fragment.setArguments(args);
         return fragment;
@@ -57,15 +58,24 @@ public class ListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_list, container, false);
+        View view;//the view being presented by this fragment
 
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_list, container, false);
+
+        //TODO: find the list, create and assign an adapter to the list, and set an onClickListener
         ListView listView = view.findViewById(R.id.listView);
 
-
-        
-
+        //onclick
+        //  bookClickedInterface.openDetails(book);
 
         return view;
     }
+
+    //This will be used to tell the activity which book has been pressed,
+    //  so that the activity can build a fragment with the proper details
+    interface bookClickedInterface{
+        void openDetails(HashMap<String, String> book);
+    }
+
 }
--- app/src/main/java/edu/temple/bookshelf/MainActivity.java
+++ app/src/main/java/edu/temple/bookshelf/MainActivity.java
@@ -8,7 +8,7 @@ import android.os.Bundle;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ListFragment.bookClickedInterface{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         Resources resources = getResources();
 
         //TODO: put this in a worker function
+        String[] keys = resources.getStringArray(R.array.keys);
         String[] bookTitles = resources.getStringArray(R.array.bookTitles); //gets the array of book titles
         String[] bookAuthors = resources.getStringArray(R.array.bookAuthors);//gets the array of book authors
 
@@ -25,10 +26,20 @@ public class MainActivity extends AppCompatActivity {
         //puts 10 hashmaps in the arraylist (all called temp) which have title and author
         for(int i = 0; i < bookTitles.length; i++){
             HashMap<String, String> temp = new HashMap<>();
-            temp.put(bookTitles[i], bookAuthors[i]);
+            temp.put(keys[0], bookTitles[i]);
+            temp.put(keys[1], bookAuthors[i]);
             books.add(temp);
         }
 
 
+    }
+
+    @Override
+    public void openDetails(HashMap<String, String> book) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.detailContainer, DetailFragment.newInstance(book))
+                .addToBackStack(null)
+                .commit();
     }
 }
